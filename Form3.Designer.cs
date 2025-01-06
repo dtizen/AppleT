00bb9e88a0c3520c03fb881d92879e3847ea5986
dtizen <126905960+dtizen@users.noreply.github.com>
2025-01-06 22:50:40 +0900

    test3

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -34,7 +34,7 @@ namespace DXApplication1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(309, 189);
+            this.button1.Location = new System.Drawing.Point(163, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
